3efca56f622476bb5101dc4dd65f1e744897f6a4
Kevin Huynh <kevinhh26@gmail.com>
2021-04-21 14:31:16 -0400

    Update login_screen.dart

--- lib/Screens/Login/login_screen.dart
+++ lib/Screens/Login/login_screen.dart
@@ -3,8 +3,98 @@ import 'package:flutter/material.dart';
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('login'),
+    return NavigationScreen();
+  }
+}
+
+/// This is the stateful widget that the main application instantiates.
+class NavigationScreen extends StatefulWidget {
+  //const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  _NavigationState createState() => _NavigationState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _NavigationState extends State<NavigationScreen> {
+  int _selectedIndex = 0;
+  static const Color barColor = Colors.white;
+  static const TextStyle optionStyle = TextStyle(
+      fontSize: 30,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+      fontFamily: 'Cookie');
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Projects',
+      style: optionStyle,
+    ),
+    Text(
+      'Teams',
+      style: optionStyle,
+    ),
+    Text(
+      'Feedback',
+      style: optionStyle,
+    ),
+    Text(
+      'Requests',
+      style: optionStyle,
+    ),
+    Text(
+      'Profile',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: _widgetOptions.elementAt(_selectedIndex),
+        backgroundColor: Colors.white,
+      ),
+      body: Center(),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assignment_outlined),
+            label: 'Projects',
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group),
+            label: 'Teams',
+            backgroundColor: Colors.black,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.feedback),
+            label: 'Feedback',
+            backgroundColor: Colors.black,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.hail),
+            label: 'Requests',
+            backgroundColor: Colors.black,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_box_rounded),
+            label: 'Profile',
+            backgroundColor: Colors.black,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
