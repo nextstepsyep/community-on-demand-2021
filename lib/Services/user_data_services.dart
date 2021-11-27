6b6256acdfad11bcc475258188dd4800d67ac2ac
Kevin Huynh <kevinhh26@gmail.com>
2021-11-27 14:40:27 -0500

    add function documentation and fix a couple of bugs

--- lib/Services/user_data_services.dart
+++ lib/Services/user_data_services.dart
@@ -9,7 +9,14 @@ CollectionReference _skills = _userData.doc(_uid).collection("skills");
 Stream<DocumentSnapshot>? _stream;
 Map<String, dynamic>? _data;
 Map<String, Map<String, dynamic>> _skillData = new Map();
-
+/**
+ * sets the User ID of the user and initializes the user data stream and
+ * user skill data stream as well as attach listeners to the stream to gather
+ * data from the database and any changes within the database which are stored
+ * in the _data data structure
+ * @param uid the User ID of the logged in account
+ * @return the stream of data from the database
+ */
 StreamSubscription<DocumentSnapshot> setUserID(String uid) {
   _uid = uid;
   _skills = _userData.doc(_uid).collection("skills");
@@ -19,22 +26,37 @@ StreamSubscription<DocumentSnapshot> setUserID(String uid) {
     list.forEach((element) {
       _skillData.putIfAbsent(element.id, () => element.data());
     });
-    print(_skillData);
-  });
+  }, onError: (e) => print('could not load user skill data'));
   _stream = _userData.doc(_uid).snapshots().asBroadcastStream();
   return _stream!.listen((event) {
     _data = event.data();
-  }, onError: (e) => print('could not load data'));
+  }, onError: (e) => print('could not load user profile data'));
 }
 
+/**
+ * gets the user ID
+ * @return the user ID
+ */
 String getUserID() {
   return _uid;
 }
 
+/**
+ * gets the user reference
+ * @return the user reference
+ */
 DocumentReference getUserReference() {
   return _userData.doc(_uid);
 }
 
+/**
+ * creates a new profile of a user by taking in their first name, last name,
+ * and bio as information and initializing thier stat values to zero which will
+ * all be sent to the database
+ * @param firstName the first name of the user
+ * @param lastName the last name of the user
+ * @param bio the bios of the user
+ */
 void createProfile(String firstName, String lastName, String bio) async {
   Map<String, dynamic> stats = {
     'used': 0,
@@ -59,6 +81,13 @@ void createProfile(String firstName, String lastName, String bio) async {
   });
 }
 
+/**
+ * creates an admin profile with the given first name, last name,
+ * and bios as information that will be sent to the database.
+ * @param firstName the first name of the admin
+ * @param lastName the last name of the admin
+ * @param bio the bios of the admin
+ */
 void createAdminProfile(String firstName, String lastName, String bio) async {
   return await _userData.doc(_uid).set({
     'firstName': firstName,
@@ -69,25 +98,53 @@ void createAdminProfile(String firstName, String lastName, String bio) async {
   });
 }
 
+/**
+ * updates the firstname, lastName, and bio of the user profile
+ * @param firstName the first name of the user
+ * @param lastName the last name of the user
+ * @param bio the bios of the user
+ */
 void updateProfile(String firstName, String lastName, String bio) {
   _userData
       .doc(_uid)
       .update({'firstName': firstName, 'lastName': lastName, 'bio': bio});
 }
 
+/**
+ * gets the user data in the form of a map data structure
+ * @return the data of the user
+ */
 Map<String, dynamic> getUserData() {
   return _data!;
 }
 
+/**
+ * gets the user skill data in the form of a map data structure
+ * @return the data of the user
+ */
 Map<String, Map<String, dynamic>> getUserSkillData() {
   return _skillData;
 }
 
+/**
+ * gets the stream of the user data which can be used in streambuilder widgets
+ * @return the stream of the user data
+ */
 Stream<DocumentSnapshot> getUserStream() {
   return _stream!;
 }
 
-void updateSkillCount(String type, int used, int didntUse, int dontHave, int totalValue) {
+/**
+ * updates the skill stats of the user after taking the card game
+ * and sends it to the database
+ * @param type the type of skill to be updated
+ * @param used number of times this type of skill was used
+ * @didnt didntUse number of time this type of skill wasn't used
+ * @param dontHave number of times this type of skill the user did not have
+ * @param totalValue the total value of the skill
+ */
+void updateSkillCount(
+    String type, int used, int didntUse, int dontHave, int totalValue) {
   DocumentReference skill = _skills.doc(type);
   Map<String, dynamic> stats = {
     'used': used,
@@ -102,11 +159,21 @@ void updateSkillCount(String type, int used, int didntUse, int dontHave, int tot
   });
 }
 
-// Returns the total value of all cards for a passed in category
+/**
+ * gets the total skill value of the particular type of skill
+ * @param category the type of skill
+ * @return the total skill value
+ */
 String getCardTotal(String category) {
   return "${getUserSkillData()[category]!['totalValue']}";
 }
 
+/**
+ * updates the steam skills stats of the user
+ * @param type the type of steam skill to be updated
+ * @param badges the number of badges to be incremented toward the total number of badges
+ * @param skillCoins the number of skills coins to be incremented toward the total number of skillcoins
+ */
 void updateSteamSkills(String type, [int badges = 0, int skillCoins = 0]) {
   DocumentReference skill = _skills.doc(type);
   int mult;
