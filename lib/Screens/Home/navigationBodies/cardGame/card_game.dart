c4f7fade50fd559a1fefa15fe471fa8646f39c68
jsmith802 <jsmith802@gatech.edu>
2021-11-27 19:07:05 -0500

    Pull card data from csv

--- lib/Screens/Home/navigationBodies/cardGame/card_game.dart
+++ lib/Screens/Home/navigationBodies/cardGame/card_game.dart
@@ -2,8 +2,9 @@ import 'package:community_on_demand_code_demo/Features/AlertMenu.dart';
 import 'package:community_on_demand_code_demo/Services/user_data_services.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:swipe_cards/swipe_cards.dart';
-
+import 'package:csv/csv.dart';
 
 class CardGameScreen extends StatefulWidget {
   @override
@@ -33,12 +34,9 @@ class CardGameScreenState extends State<CardGameScreen> {
         backgroundColor: Colors.white,
       ),
       // stuff
-      body: Center(
-          child: CardGame()
-      ),
+      body: Center(child: CardGame()),
     );
   }
-
 }
 
 class CardGame extends StatefulWidget {
@@ -48,417 +46,51 @@ class CardGame extends StatefulWidget {
 
 class _CardGameState extends State<CardGame> {
   List<SwipeItem> _swipeItems = <SwipeItem>[];
-  late MatchEngine _matchEngine;
+  MatchEngine? _matchEngine;
   GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
   Map<String, List<int>> categoryTotals = {};
+  List<Card> cards = [];
 
-  List<Card> cards = [
-    //awareness: 5 cards
-    Card(
-      name: 'career awareness',
-      type: 'awareness',
-      imgUrl: 'assets/images/cards/awareness - career awareness.jpg',
-      value: 10,
-    ),
-    Card(
-      name: 'career exploration',
-      type: 'awareness',
-      imgUrl: 'assets/images/cards/awareness - career exploration.jpg',
-      value: 10,
-    ),
-    Card(
-      name: 'fafsa',
-      type: 'awareness',
-      imgUrl: 'assets/images/cards/awareness - fafsa.jpg',
-      value: 10,
-    ),
-    Card(
-      name: 'mission statement',
-      type: 'awareness',
-      imgUrl: 'assets/images/cards/awareness - mission statement.jpg',
-      value: 10,
-    ),
-    Card(
-      name: 'salary',
-      type: 'awareness',
-      imgUrl: 'assets/images/cards/awareness - salary.jpg',
-      value: 10,
-    ),
+  Future<List<Card>> getCards() async {
+    if (cards.isNotEmpty) {
+      return cards;
+    } else {
+      final _rawData = await rootBundle.loadString('assets/data/Card Data.csv');
+      List<List<dynamic>> _listData = CsvToListConverter().convert(_rawData);
+      _listData.removeAt(0);
+      List<Card> cardList = [];
+      for (List<dynamic> item in _listData) {
+        String cardName = item[0];
+        String cardType = item[1];
+        int cardValue = item[2];
+        String cardImgUrl = 'assets/images/cards/' + item[3];
+        Card aCard = Card(
+            name: cardName,
+            type: cardType,
+            value: cardValue,
+            imgUrl: cardImgUrl);
+        cardList.add(aCard);
+      }
+      return cardList;
+    }
+  }
 
-    //innovation: 12 cards
-    Card(
-      name: 'competitive advantage',
-      type: 'innovation',
-      imgUrl: 'assets/images/cards/innovation - competitive advantage.jpg',
-      value: 20,
-    ),
-    Card(
-      name: 'copyrights',
-      type: 'innovation',
-      imgUrl: 'assets/images/cards/innovation - copyrights.jpg',
-      value: 20,
-    ),
-    Card(
-      name: 'education program',
-      type: 'innovation',
-      imgUrl: 'assets/images/cards/innovation - education program.jpg',
-      value: 20,
-    ),
-    Card(
-      name: 'entertainment program',
-      type: 'innovation',
-      imgUrl: 'assets/images/cards/innovation - entertainment program.jpg',
-      value: 20,
-    ),
-    Card(
-      name: 'guest services program',
-      type: 'innovation',
-      imgUrl: 'assets/images/cards/innovation - guest services programs.jpg',
-      value: 20,
-    ),
-    Card(
-      name: 'operations',
-      type: 'innovation',
-      imgUrl: 'assets/images/cards/innovation - operations.png',
-      value: 20,
-    ),
-    Card(
-      name: 'product life cycle',
-      type: 'innovation',
-      imgUrl: 'assets/images/cards/innovation - product life cycle.jpg',
-      value: 20,
-    ),
-    Card(
-      name: 'products _ services i',
-      type: 'innovation',
-      imgUrl: 'assets/images/cards/innovation - products _ services i.jpg',
-      value: 20,
-    ),
-    Card(
-      name: 'products _ services ii',
-      type: 'innovation',
-      imgUrl: 'assets/images/cards/innovation - products _ services ii.jpg',
-      value: 20,
-    ),
-    Card(
-      name: 'research',
-      type: 'innovation',
-      imgUrl: 'assets/images/cards/innovation - research.png',
-      value: 20,
-    ),
-    Card(
-      name: 'speaker program',
-      type: 'innovation',
-      imgUrl: 'assets/images/cards/innovation - speaker program.jpg',
-      value: 20,
-    ),
-    Card(
-      name: 'supplier',
-      type: 'innovation',
-      imgUrl: 'assets/images/cards/innovation - supplier.png',
-      value: 20,
-    ),
+  Future<Map<String, List<int>>> getCategoryTotals(List<Card> cardList) async {
+    Map<String, List<int>> catTotals = {};
+    for (Card card in cardList) {
+      if (!catTotals.containsKey(card.type)) {
+        catTotals[card.type] = [0, 0, 0, 0];
+      }
+    }
+    return catTotals;
+  }
 
-    //lead: 11 cards
-    Card(
-      name: 'communications director',
-      type: 'lead',
-      imgUrl: 'assets/images/cards/lead - communications director.jpg',
-      value: 50,
-    ),
-    Card(
-      name: 'community ecosystem',
-      type: 'lead',
-      imgUrl: 'assets/images/cards/lead - community ecosystem.jpg',
-      value: 50,
-    ),
-    Card(
-      name: 'conflict resolution',
-      type: 'lead',
-      imgUrl: 'assets/images/cards/lead - conflict resolution.jpg',
-      value: 50,
-    ),
-    Card(
-      name: 'culture',
-      type: 'lead',
-      imgUrl: 'assets/images/cards/lead - culture.jpg',
-      value: 50,
-    ),
-    Card(
-      name: 'family _ peers',
-      type: 'lead',
-      imgUrl: 'assets/images/cards/lead - family _ peers.jpg',
-      value: 50,
-    ),
-    Card(
-      name: 'finance director',
-      type: 'lead',
-      imgUrl: 'assets/images/cards/lead - finance director.jpg',
-      value: 50,
-    ),
-    Card(
-      name: 'general manager',
-      type: 'lead',
-      imgUrl: 'assets/images/cards/lead - general manager.jpg',
-      value: 50,
-    ),
-    Card(
-      name: 'leadership',
-      type: 'lead',
-      imgUrl: 'assets/images/cards/lead - leadership.jpg',
-      value: 50,
-    ),
-    Card(
-      name: 'network director',
-      type: 'lead',
-      imgUrl: 'assets/images/cards/lead - network director.jpg',
-      value: 50,
-    ),
-    Card(
-      name: 'team operations',
-      type: 'lead',
-      imgUrl: 'assets/images/cards/lead - team operations.jpg',
-      value: 50,
-    ),
-    Card(
-      name: 'workshops',
-      type: 'lead',
-      imgUrl: 'assets/images/cards/lead - workshops.jpg',
-      value: 50,
-    ),
-
-    //skill: 18 cards
-    Card(
-      name: 'agriculture',
-      type: 'skill',
-      imgUrl: 'assets/images/cards/skill - agriculture.jpg',
-      value: 40,
-    ),
-    Card(
-      name: 'architecture',
-      type: 'skill',
-      imgUrl: 'assets/images/cards/skill - architecture.jpg',
-      value: 40,
-    ),
-    Card(
-      name: 'art',
-      type: 'skill',
-      imgUrl: 'assets/images/cards/skill - art.jpg',
-      value: 40,
-    ),
-    Card(
-      name: 'business',
-      type: 'skill',
-      imgUrl: 'assets/images/cards/skill - business.jpg',
-      value: 40,
-    ),
-    Card(
-      name: 'education',
-      type: 'skill',
-      imgUrl: 'assets/images/cards/skill - education.jpg',
-      value: 40,
-    ),
-    Card(
-      name: 'film',
-      type: 'skill',
-      imgUrl: 'assets/images/cards/skill - film.jpg',
-      value: 40,
-    ),
-    Card(
-      name: 'finance',
-      type: 'skill',
-      imgUrl: 'assets/images/cards/skill - finance.jpg',
-      value: 40,
-    ),
-    Card(
-      name: 'government',
-      type: 'skill',
-      imgUrl: 'assets/images/cards/skill - government.jpg',
-      value: 40,
-    ),
-    Card(
-      name: 'green energy',
-      type: 'skill',
-      imgUrl: 'assets/images/cards/skill - green energy.jpg',
-      value: 40,
-    ),
-    Card(
-      name: 'health sciences',
-      type: 'skill',
-      imgUrl: 'assets/images/cards/skill - health sciences.jpg',
-      value: 40,
-    ),
-    Card(
-      name: 'hospitality',
-      type: 'skill',
-      imgUrl: 'assets/images/cards/skill - hospitality.jpg',
-      value: 40,
-    ),
-    Card(
-      name: 'human services',
-      type: 'skill',
-      imgUrl: 'assets/images/cards/skill - human services.jpg',
-      value: 40,
-    ),
-    Card(
-      name: 'info tech',
-      type: 'skill',
-      imgUrl: 'assets/images/cards/skill - info tech.jpg',
-      value: 40,
-    ),
-    Card(
-      name: 'law',
-      type: 'skill',
-      imgUrl: 'assets/images/cards/skill - law.jpg',
-      value: 40,
-    ),
-    Card(
-      name: 'manufacturing',
-      type: 'skill',
-      imgUrl: 'assets/images/cards/skill - manufacturing.jpg',
-      value: 40,
-    ),
-    Card(
-      name: 'marketing',
-      type: 'skill',
-      imgUrl: 'assets/images/cards/skill - marketing.jpg',
-      value: 40,
-    ),
-    Card(
-      name: 'STEAM IP',
-      type: 'skill',
-      imgUrl: 'assets/images/cards/skill - STEAM IP.jpg',
-      value: 40,
-    ),
-    Card(
-      name: 'transportation',
-      type: 'skill',
-      imgUrl: 'assets/images/cards/skill - transportation.jpg',
-      value: 40,
-    ),
-
-    //workforce:
-    Card(
-      name: 'advancement',
-      type: 'workforce',
-      imgUrl: 'assets/images/cards/workforce - advancement.jpg',
-      value: 30,
-    ),
-    Card(
-      name: 'appearance',
-      type: 'workforce',
-      imgUrl: 'assets/images/cards/workforce - appearance.jpg',
-      value: 30,
-    ),
-    Card(
-      name: 'budget',
-      type: 'workforce',
-      imgUrl: 'assets/images/cards/workforce - budget.jpg',
-      value: 30,
-    ),
-    Card(
-      name: 'contributions',
-      type: 'workforce',
-      imgUrl: 'assets/images/cards/workforce - contributions.jpg',
-      value: 30,
-    ),
-    Card(
-      name: 'effective communications',
-      type: 'workforce',
-      imgUrl: 'assets/images/cards/workforce - effective communications.jpg',
-      value: 30,
-    ),
-    Card(
-      name: 'feedback',
-      type: 'workforce',
-      imgUrl: 'assets/images/cards/workforce - feedback.jpg',
-      value: 30,
-    ),
-    Card(
-      name: 'help',
-      type: 'workforce',
-      imgUrl: 'assets/images/cards/workforce - help.jpg',
-      value: 30,
-    ),
-    Card(
-      name: 'job market',
-      type: 'workforce',
-      imgUrl: 'assets/images/cards/workforce - job market.jpg',
-      value: 30,
-    ),
-    Card(
-      name: 'job search',
-      type: 'workforce',
-      imgUrl: 'assets/images/cards/workforce - job search.jpg',
-      value: 30,
-    ),
-    Card(
-      name: 'mock interviews',
-      type: 'workforce',
-      imgUrl: 'assets/images/cards/workforce - mock interviews.jpg',
-      value: 30,
-    ),
-    Card(
-      name: 'negotiations',
-      type: 'workforce',
-      imgUrl: 'assets/images/cards/workforce - negotiations.jpg',
-      value: 30,
-    ),
-    Card(
-      name: 'resume',
-      type: 'workforce',
-      imgUrl: 'assets/images/cards/workforce - resume.jpg',
-      value: 30,
-    ),
-    Card(
-      name: 'takes initiative',
-      type: 'workforce',
-      imgUrl: 'assets/images/cards/workforce - takes initiative.jpg',
-      value: 30,
-    ),
-    Card(
-      name: 'thank you',
-      type: 'workforce',
-      imgUrl: 'assets/images/cards/workforce - thank you.jpg',
-      value: 30,
-    ),
-    Card(
-      name: 'time management',
-      type: 'workforce',
-      imgUrl: 'assets/images/cards/workforce - time management.jpg',
-      value: 30,
-    ),
-    Card(
-      name: 'transition',
-      type: 'workforce',
-      imgUrl: 'assets/images/cards/workforce - transition.jpg',
-      value: 30,
-    ),
-    Card(
-      name: 'workplace',
-      type: 'workforce',
-      imgUrl: 'assets/images/cards/workforce - workplace.jpg',
-      value: 30,
-    ),
-  ];
-
-  @override
-  void initState() {
-    categoryTotals = {
-      'awareness': [0, 0, 0, 0], // [didn't have or use, had but didn't use, had and used, total points]
-      'innovation': [0, 0, 0, 0],
-      'lead': [0, 0, 0, 0],
-      'skill': [0, 0, 0, 0],
-      'workforce': [0, 0, 0, 0],
-    };
+  Future<void> initializeSwipeItems() async {
+    cards = await getCards();
+    categoryTotals = await getCategoryTotals(cards);
     for (int i = 0; i < cards.length; i++) {
       _swipeItems.add(SwipeItem(
-          content: Card(
-              name: cards[i].name,
-              type: cards[i].type,
-              imgUrl: cards[i].imgUrl,
-              value: cards[i].value,
-          ),
+          content: cards[i],
           likeAction: () {
             cards[i].skillLevel = 2;
             categoryTotals[cards[i].type]![2] += 1;
@@ -476,74 +108,77 @@ class _CardGameState extends State<CardGame> {
           }));
     }
 
-    _matchEngine = MatchEngine(swipeItems: _swipeItems);
+    setState(() {
+      _matchEngine = MatchEngine(swipeItems: _swipeItems);
+    });
+  }
+
+  @override
+  void initState() {
     super.initState();
+    initializeSwipeItems();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         key: _scaffoldKey,
-        body: Container(
-            child: Column(children: [
-
-
-
-              Container(
-                height: 550,
-                child: SwipeCards(
-                  matchEngine: _matchEngine,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      alignment: Alignment.center,
-                      child: SingleChildScrollView(
-                        child: Column(
-                          children: <Widget>[
-                            Image.asset(_swipeItems[index].content.imgUrl),
-                          ],
+        body: _matchEngine != null
+            ? Container(
+                child: Column(children: [
+                Container(
+                  height: 550,
+                  child: SwipeCards(
+                    matchEngine: _matchEngine!,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        alignment: Alignment.center,
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: <Widget>[
+                              Image.asset(_swipeItems[index].content.imgUrl),
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-                  },
-                  onStackFinished: () {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) =>
-                            AlertMenu(
-                              content: "Finished",
-                              actions: [],
-                            )
-                    );
-                    categoryTotals.forEach((key, value) {
-                      updateSkillCount(key, value[2], value[1], value[0], value[3]);
-                    });
-                    print(categoryTotals);
-                  },
+                      );
+                    },
+                    onStackFinished: () {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) => AlertMenu(
+                                content: "Finished",
+                                actions: [],
+                              ));
+                      categoryTotals.forEach((key, value) {
+                        updateSkillCount(
+                            key, value[2], value[1], value[0], value[3]);
+                      });
+                    },
+                  ),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  RaisedButton(
-                      onPressed: () {
-                        _matchEngine.currentItem!.nope();
-                      },
-                      child: Text("Don't Have")),
-                  RaisedButton(
-                      onPressed: () {
-                        _matchEngine.currentItem!.superLike();
-                      },
-                      child: Text("Somewhat Have")),
-                  RaisedButton(
-                      onPressed: () {
-                        _matchEngine.currentItem!.like();
-                      },
-                      child: Text("Have"))
-                ],
-              )
-            ])));
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {
+                          _matchEngine!.currentItem!.nope();
+                        },
+                        child: Text("Don't Have")),
+                    ElevatedButton(
+                        onPressed: () {
+                          _matchEngine!.currentItem!.superLike();
+                        },
+                        child: Text("Somewhat Have")),
+                    ElevatedButton(
+                        onPressed: () {
+                          _matchEngine!.currentItem!.like();
+                        },
+                        child: Text("Have"))
+                  ],
+                )
+              ]))
+            : Text("Loading"));
   }
-  
 }
 
 class Card {
@@ -560,6 +195,9 @@ class Card {
     required this.value,
     skillLevel = 0,
   });
+
+  @override
+  String toString() {
+    return "{ name : ${this.name}, type: ${this.type}, value: ${this.value}, imgUrl: ${this.imgUrl}, skillLevel: ${this.skillLevel} }";
+  }
 }
-
-
